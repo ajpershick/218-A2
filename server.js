d28b92c2b20b8e6d6015415a85ec4ac9a99f508a
vberezny <v.bereznyakov@gmail.com>
2018-02-17 18:55:06 -0800

    some progress

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ server.on('request', function(req,res){
   console.log('request:', req.url);
   var urlObj = url.parse(req.url, true); // true => query turned into an obj
   console.log(urlObj.query.lname);
-  if (req.method === 'GET' && req.url.match(/^\/.+\.html$/)){
-    var filepath = path.join(__dirname,req.url);
+  if (req.method === 'GET' && req.url === '/'){
+    var filepath = path.join(__dirname, './form.html');
 
     fs.readFile(filepath, function(err, contents){
       if(err){
@@ -24,23 +24,34 @@ server.on('request', function(req,res){
 
     });
 
-  } else if (req.method === 'GET' && req.url.match(/^\/.+\.jpg$/)){
+  } else if (req.method === 'GET' && req.url === '/data/users.json'){
+    var filepath = path.join(__dirname, './data/users.json');
 
-    var imgpath = path.join(__dirname,req.url);
-    var imgstream = fs.createReadStream(imgpath, { highWaterMark: 1024 });
-    res.writeHead(200, {"Content-Type": "image/jpeg"});
-    imgstream.pipe(res);
+    fs.readFile(filepath, function(err, contents){
+      if(err){
+        // handle error
+      } else {
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.write(contents);
+        res.end();
+      }
 
-  } else if (req.method === 'POST' && req.url === '/'){
-    var body ='';
-    req.on('data', function(data){
-      body += data.toString();
     });
-    req.on('end', function(){
-      var postObj = qs.parse(body);
-      console.log(postObj);
-      res.end();
+  } else if (req.method === 'GET' && req.url === '/users.html'){
+    var filepath = path.join(__dirname, './users.html');
+
+    fs.readFile(filepath, function(err, contents){
+      if(err){
+        // handle error
+      } else {
+        res.writeHead(200, {"Content-Type": "html/text"});
+        res.write(contents);
+        res.end();
+      }
+
     });
+  }else if (req.method === 'POST' && req.url === '/'){
+
   } else {
     res.writeHead(404);
     res.write('404 Error');
