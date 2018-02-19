6ba0afc8073eafd556a146a3482fae8998221540
vberezny <v.bereznyakov@gmail.com>
2018-02-18 22:36:06 -0800

    kinda finished

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
-var url = require('url');
 var qs = require('querystring');
 
 var server = http.createServer();
 server.on('request', function(req,res){
 
-  console.log('request:', req.url);
-  var urlObj = url.parse(req.url, true); // true => query turned into an obj
-  //console.log(urlObj);
   if (req.method === 'GET' && req.url === '/'){
     var filepath = path.join(__dirname, './form.html');
 
@@ -24,7 +20,21 @@ server.on('request', function(req,res){
         res.end();
       }
     });
-  } else if (req.method === 'GET' && req.url === '/data/users.json'){
+  } else if (req.method === 'GET' && req.url === '/users.html'){
+    var filepath = path.join(__dirname, './users.html');
+
+    fs.readFile(filepath, function(err, contents){
+      if(err){
+        res.writeHead(404);
+        res.write('404 Error');
+        res.end()
+      } else {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write(contents);
+        res.end();
+      }
+    });
+  }else if (req.method === 'GET' && req.url === '/data/users.json'){
     var filepath = path.join(__dirname, './data/users.json');
 
     fs.readFile(filepath, function(err, contents){
@@ -40,6 +50,20 @@ server.on('request', function(req,res){
     });
   } else if (req.method === 'GET' && req.url === '/script.js'){
     var filepath = path.join(__dirname, './script.js');
+
+    fs.readFile(filepath, function(err, contents){
+      if(err){
+        res.writeHead(404);
+        res.write('404 Error');
+        res.end()
+      } else {
+        res.writeHead(200, {"Content-Type": "text/javascript"});
+        res.write(contents);
+        res.end();
+      }
+    });
+  }else if (req.method === 'GET' && req.url === '/users.js'){
+    var filepath = path.join(__dirname, './users.js');
 
     fs.readFile(filepath, function(err, contents){
       if(err){
@@ -73,8 +97,11 @@ server.on('request', function(req,res){
     });
     req.on('end', function(){
       var postObj = qs.parse(body);
-      console.log(postObj);
-      fs.writeFile("./data/users.json", JSON.stringify(postObj, null, 4));
+      fs.readFile("./data/users.json", function(err, data){
+        var json = JSON.parse(data);
+        json.users.push(postObj);
+        fs.writeFile("./data/users.json", JSON.stringify(json, null, 4));
+      });
       res.end();
     });
   } else {
@@ -83,6 +110,4 @@ server.on('request', function(req,res){
     res.end()
   }
 });
-server.listen(8080);
-
-console.log('Magic is happening on port 8080');
+server.listen(26568);
